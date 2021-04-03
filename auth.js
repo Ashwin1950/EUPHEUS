ccfff3168448a0a629f56272e9620a55243cd4f3
Ashwin Muthu Kumar S <67495595+Ashwin1950@users.noreply.github.com>
2021-04-04 01:49:58 +0530

    added html pages 1

---
+++ auth.js
@@ -0,0 +1,30 @@
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // get user info
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+  // sign up the user
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    // close the signup modal & reset form
+   console.log(cred.user)
+   alert("Signup Successful :)")
+            var link='index.html';
+            window.location.assign(link);
+
+  });
+});
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
---
+++ form.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBQMGv4opFWBZIQwz9wNlMnfaWmXsMBLqk",
+    authDomain: "eupheus-d7976.firebaseapp.com",
+    projectId: "eupheus-d7976",
+    storageBucket: "eupheus-d7976.appspot.com",
+    messagingSenderId: "1038134148139",
+    appId: "1:1038134148139:web:87e23d80e4690df0c0fe6e",
+    measurementId: "G-BSEFH4C1GE"
+  };
+  firebase.initializeApp(config);
+  
+  
+ 
